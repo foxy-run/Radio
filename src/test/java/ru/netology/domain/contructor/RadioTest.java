330a0a536128dce5830aafcd23f42ee59ef2dbd4
Xenia Bocharova <bochxenia@yandex.ru>
2020-11-15 19:47:21 +0300

    Add Constuctor changes

--- src/test/java/ru/netology/domain/contructor/RadioTest.java
+++ src/test/java/ru/netology/domain/contructor/RadioTest.java
@@ -7,26 +7,26 @@ class RadioTest {
     @Test
         public void shouldCreate() {
         Radio radio = new Radio(8,0,7,100,0,89);
-        System.out.println("hi");
+
 }
     @Test
     public void shouldCreate1() {
         Radio radio = new Radio();
-        System.out.println("hi");
+
     }
     @Test
     public void shouldCreate2() {
         Radio radio = new Radio(7, 8 );
-        System.out.println("hi");
+
     }
     @Test
     public void shouldCreate3() {
         Radio radio = new Radio(77,2,58);
-        System.out.println("hi");
+
     }
     @Test
     public void shouldCreate4() {
         Radio radio = new Radio();
-        System.out.println("hi");
+
     }
 }
