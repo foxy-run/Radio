b0d42ea8158eefbfc8efc4838d5050a443e250bd
Xenia Bocharova <bochxenia@yandex.ru>
2020-11-15 20:37:34 +0300

    Add lombok changes

--- src/test/java/ru/netology/domain/contructor/RadioTest.java
+++ src/test/java/ru/netology/domain/contructor/RadioTest.java
@@ -5,28 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
     @Test
-        public void shouldCreate() {
-        Radio radio = new Radio(8,0,7,100,0,89);
-
-}
-    @Test
-    public void shouldCreate1() {
+    public void shouldCreate() {
         Radio radio = new Radio();
 
-    }
-    @Test
-    public void shouldCreate2() {
-        Radio radio = new Radio(7, 8 );
-
-    }
-    @Test
-    public void shouldCreate3() {
-        Radio radio = new Radio(77,2,58);
-
-    }
-    @Test
-    public void shouldCreate4() {
-        Radio radio = new Radio();
 
     }
 }
