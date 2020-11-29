84d720a64baa4f08be878826a2e82535aa9dce9e
Xenia Bocharova <bochxenia@yandex.ru>
2020-11-29 16:47:59 +0300

    add change

--- src/main/java/ru/netology/domain/contructor/Radio.java
+++ src/main/java/ru/netology/domain/contructor/Radio.java
@@ -1,7 +1,7 @@
 package ru.netology.domain.contructor;
 
 public class Radio {
-    private int maxStation = 14;
+    private int maxStation = 10;
     private int minStation = 0;
     private int currentStation;
     private int maxVolume = 100;
@@ -16,10 +16,106 @@ public class Radio {
         this.minVolume = minVolume;
         this.currentVolume = currentVolume;
     }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextCurrentStation() {
+        int currentStation = this.currentStation;
+        if (currentStation >= maxStation) {
+            this.currentStation = minStation;
+        } else {
+            this.currentStation = currentStation + 1;
+        }
+    }
+
+    public void prevCurrentStation() {
+        int currentStation = this.currentStation;
+        if (currentStation <= minStation) {
+            this.currentStation = maxStation;
+        } else {
+            this.currentStation = currentStation - 1;
+        }
+    }
+
+    public void remoteCurrentStation() {
+        if (currentStation > maxStation) {
+            this.currentStation = maxStation;
+        } else if (currentStation < minStation) {
+            this.currentStation = minStation;
+        } else {
+            this.currentStation = currentStation;
+        }
+    }
+
+    // Громкость
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume() {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void nextCurrentVolume() {
+        int currentVolume = this.currentVolume;
+        if (currentVolume >= maxVolume) {
+            this.currentVolume = maxVolume;
+        } else {
+            this.currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void prevCurrentVolume() {
+        int currentVolume = this.currentVolume;
+        if (currentVolume <= minVolume) {
+            this.currentVolume = minVolume;
+        } else {
+            this.currentVolume = currentVolume - 1;
+        }
+    }
+
+
     public Radio() {
     }
 
-    public Radio(int maxStation, int minStation, int maxVolume, int minVolume) {
+    }
+
+    /*public Radio(int maxStation, int minStation, int maxVolume, int minVolume) {
         this.maxStation = maxStation;
         this.minStation = minStation;
         this.maxVolume = maxVolume;
@@ -37,7 +133,7 @@ public class Radio {
     public Radio(int currentStation, int currentVolume) {
         this.currentStation = currentStation;
         this.currentVolume = currentVolume;
-    }
+    }*/
 
-      }
+
 
